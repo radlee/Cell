933c89ec50f823fdff01a92a64be422b31cbf0b2
radlee <lolito@projectcodex.co>
2016-04-07 13:18:57 +0200

    Cell-Getting Data

---
+++ cell.js
@@ -0,0 +1,36 @@
+var walter = "Walter sent: “It was 67MB. Is that everything? I’m busy, you know.”"
+var jesse = "Jesse sent: “Damn, it was like 300MB. Dat Snaptalk is a data hog, yo.”"
+var saul = "Saul sent: “I think 283MB. That’s what the guy at the store said. I think it means MegaBites.”"
+var gus = "Gus sent: “Around 150MB. I have the receipts if you need more precise figures.”"
+
+var waltersApps = "“Hello. I’ve got Faceblock and Instantgam. I’m too old for the other ones.”"
+var jessesApps = "“Yo! I’ve got ‘em all. Tooter, Faceblock, Instantgam, Snaptalk.”"
+var saulsApps = "“I’m on LankedOn for Business. And I have an Instantgam account for my cats. Oh, and Faceblock for my ex-husbands.”"
+var gussApps = "“I use Tooter and Instantgam. My customers aren’t on the others, so I can’t get them to engage with my brand there.”"
+
+//Apps
+var Faceblock = "Faceblock"
+var Instantgam = "Instantgam"
+var Tooter = "Tooter"
+var Snaptalk = "Snaptalk"
+var LankedOn = "LankedOn"
+
+//Get Data for each person 
+var getData = function(stringData, name){
+	var data = stringData.replace(/\D/g, '');
+	console.log(name + " -> " + data);
+	return data;
+}
+
+//Get Apps for Each Person
+var getApps = function(stringApps, name){
+	var data = stringApps.replace(/Faceblock/g, '');
+	console.log(data)
+}
+
+getApps(walter, 'walter')
+
+getData(walter, 'walter')
+getData(jesse, 'jesse')
+getData(saul, 'saul')
+getData(gus, 'gus')
