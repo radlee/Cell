3fc31bd033e71a2e73e0bf6a727f3c07f2a199b3
radlee <lolito@projectcodex.co>
2016-04-09 22:58:00 +0200

    Read Apps from a File

---
+++ Cell/README_CELLFILES_AND_APPS.md
@@ -0,0 +1,9 @@
+# Cell
+Cell Kata
+
+
+The cellFiles.js uses the apps.txt as an input of users apps. 
+It reads the lines and stores all the app names in an array, the problem is that it stores all of each line's apps in one list.
+
+The idea is to have 4 separate lists with the right input of
+apps in each one of them. 
--- Cell/cellFiles.js
+++ Cell/cellFiles.js
@@ -1,46 +1,90 @@
 var fs = require('fs');
-console.log("----Execute Asynchronous Read----- ")
-var stringData = fs.readFileSync("input.txt", 'utf8');
-
-//Splitting Method
+fs.readFile("apps.txt", 'utf8', function(err, data){
+	if(err) throw err;
+	
+	//Splitting Method
 var splitString = function(stringToSplit, separator){
 	var arrayOfStrings = stringToSplit.split(separator);
 	return arrayOfStrings;
 }
 
-var setsString = splitString(stringData, ' ');
 
-console.log("These are the lines")
-// console.log(setsString)
+console.log(setsString)
 
 
+//Remove all panctuation from the string for Walter
+var cleanString =[];
+var setsString = splitString(data, ' ');
+var removed = setsString.map(function(word){
+	var repl = word.replace(/[.”,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	var repl = repl.replace(/\s+/g,"");
+	cleanString.push(repl);
+})
 
-var getPairedArray = function(setsString, split){
-	var setsArr = [];
-	for(var i =0; i< setsString.length; i++){
-		var set = setsString[i].split(split)
-		setsArr.push(set)
+//store apps for walter
+var waltersApps = [];
+console.log("\nWalter's Apps!-------")
+var words = cleanString.map(function(word){
+
+	if(word === "Faceblock"){
+		waltersApps.push(word)
 	}
-	console.log("Array of Sets")
-	return setsArr;
-}
+	if(word === "Instantgam"){
+		waltersApps.push(word)
+	}
+	if(word === "LankedOn"){
+		waltersApps.push(word)
+	}
+	if(word === "Snaptalk"){
+		waltersApps.push(word)
+	}
+	if(word === "Tooter"){
+		waltersApps.push(word)
+	}
+})
+console.log( waltersApps)
 
-var paried = getPairedArray(setsString, ',');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var getPairedArray = function(setsString, split){
+// 	var setsArr = [];
+// 	for(var i =0; i< setsString.length; i++){
+// 		var set = setsString[i].split(split)
+// 		setsArr.push(set)
+// 	}
+// 	console.log("Array of Sets")
+// 	return setsArr;
+// }
+
+// var paried = getPairedArray(setsString, '\n');
 
 // console.log(paried)
 
 
 
-var dataList =[];
-paried.forEach(function(item){
+// var dataList =[];
+// paried.forEach(function(item){
 
-	// console.log(item)
-	var item = String(item)
-	var data = item.replace(/\D/g, '');
-	// var data = item.replace("", '');
-	dataList.push(data);
-	// console.log(" -> " + data);
-})
-console.log("Data----------------")
-console.log(dataList)
+// 	console.log(item)
+// 	var item = String(item)
+// 	var data = item.replace(/\D/g, '');
+// 	// var data = item.replace("", '');
+// 	dataList.push(data);
+// 	console.log(" -> " + data);
+// })
+// console.log("Data----------------")
+// console.log(paried)
 
